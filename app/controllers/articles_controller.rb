8025eeb1f0918b48faecf63629c29efd4a2b10c9
Philipp Thurner <philipp_thurner@hotmail.com>
2017-03-04 01:45:29 +0000

    Add index, update and show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,19 @@
 class ArticlesController < ApplicationController
+    
+    def index
+        @articles = Article.all
+    end
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+    
     def new
         @article = Article.new
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
     end
     
     def create
@@ -16,9 +29,29 @@ class ArticlesController < ApplicationController
         
     end
     
-    def show
-        @article = Article.find(params[:id])
+   
+    def update
+   
+       @article = Article.find(params[:id])
+       if @article.update(article_params)
+         flash[:notice] = "Article was succesfully updated"
+            redirect_to article_path(@article)
+        else
+            #flash[:error] = @article.errors.full_messages 
+            render 'edit'
+       end
     end
+    
+    def destroy
+        @article = Article.find(params[:id])
+        if @article.destroy
+            flash[:notice] = "Article was succesfully deleted"
+            redirect_to articles_path
+        else
+            flash[:notice] = "Error deleting article"
+        end
+    end
+    
     
     private
         def article_params
